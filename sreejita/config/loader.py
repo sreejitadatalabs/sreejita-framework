6788df9a81a66790d7a2beb1ad7cf3d2577edcee
sreejitadatalabs <sreejitadatalabs@gmail.com>
2025-12-13 19:30:14 +0530

    Update loader.py

--- sreejita/config/loader.py
+++ sreejita/config/loader.py
@@ -2,14 +2,13 @@ import yaml
 from .defaults import DEFAULT_CONFIG
 
 def load_config(path: str) -> dict:
-    with open(path, "r") as f:
-        user_config = yaml.safe_load(f)
+    with open(path) as f:
+        user = yaml.safe_load(f)
 
-    # merge defaults
     config = DEFAULT_CONFIG.copy()
-    for key, value in user_config.items():
-        if isinstance(value, dict):
-            config[key] = {**config.get(key, {}), **value}
+    for k, v in user.items():
+        if isinstance(v, dict):
+            config[k] = {**config.get(k, {}), **v}
         else:
-            config[key] = value
+            config[k] = v
     return config
