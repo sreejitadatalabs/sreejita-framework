62f767bc964927548769f619f51f07d81c00acf9
Arasavalli Yeswanth Nagasai <132380899+YeswanthArasavalli@users.noreply.github.com>
2025-12-13 23:25:15 +0530

    Update test_batch_runner.py

--- tests/test_batch_runner.py
+++ tests/test_batch_runner.py
@@ -1,3 +1,8 @@
 def test_batch_runner_import():
     from sreejita.automation.batch_runner import run_batch
     assert callable(run_batch)
+
+
+def test_single_file_runner_import():
+    from sreejita.automation.batch_runner import run_single_file
+    assert callable(run_single_file)
