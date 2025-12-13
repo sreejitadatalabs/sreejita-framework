d054867f9a5c1a426b6dc1ed6d3001725b82d389
Arasavalli Yeswanth Nagasai <132380899+YeswanthArasavalli@users.noreply.github.com>
2025-12-13 23:41:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,32 @@ from setuptools import setup, find_packages
 
 setup(
     name="sreejita-framework",
-    version="1.2.0",
+    version="1.6.0",
     packages=find_packages(),
     install_requires=[
-        "pandas","numpy","pyyaml","scikit-learn"
+        "pandas>=2.0",
+        "numpy>=1.24",
+        "matplotlib>=3.7",
+        "seaborn>=0.12",
+        "reportlab>=4.0",
+        "python-dateutil>=2.8",
+        "pyaml>=23.7",
+        "watchdog>=3.0",
+        "psutil>=5.9"
     ],
-    entry_points={
-        "console_scripts": [
-            "sreejita=sreejita.cli:main"
-        ]
-    }
+    python_requires=">=3.8",
+    author="Yeswanth Arasavalli",
+    description="Universal Data Analytics & Reporting Engine with Quality Assurance",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sreejitadatalabs/sreejita-framework",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ]
 )
