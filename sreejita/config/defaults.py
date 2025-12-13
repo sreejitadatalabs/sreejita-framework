d6deb2e577d0c554383b385993d248f998ed2a3b
Arasavalli Yeswanth Nagasai <132380899+YeswanthArasavalli@users.noreply.github.com>
2025-12-13 12:59:31 +0530

    Create defaults.py

---
+++ sreejita/config/defaults.py
@@ -0,0 +1,15 @@
+DEFAULT_CONFIG = {
+    "dataset": {
+        "date": None,
+        "sales": "sales",
+        "profit": "profit"
+    },
+    "analysis": {
+        "categorical": [],
+        "numeric_distribution": []
+    },
+    "report": {
+        "mode": "dynamic",
+        "output": "report.pdf"
+    }
+}
