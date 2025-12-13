db86f56158b82c4c1b1892fbb0146dd14af34452
sreejitadatalabs <sreejitadatalabs@gmail.com>
2025-12-13 19:37:39 +0530

    Create router.py

---
+++ sreejita/domains/router.py
@@ -0,0 +1,20 @@
+from sreejita.domains import retail, ecommerce, customer, text
+
+DOMAIN_REGISTRY = {
+    "retail": retail,
+    "ecommerce": ecommerce,
+    "customer": customer,
+    "text": text,
+    "generic": None
+}
+
+def apply_domain_enrichment(df, domain_name: str):
+    domain = DOMAIN_REGISTRY.get(domain_name)
+
+    if domain is None:
+        return df
+
+    if hasattr(domain, "enrich"):
+        return domain.enrich(df)
+
+    return df
