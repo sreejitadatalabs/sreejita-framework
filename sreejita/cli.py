0ff384fb614eb4fcc0ecb9451ebe96d0812f8cba
sreejitadatalabs <sreejitadatalabs@gmail.com>
2025-12-13 20:26:54 +0530

    Update cli.py

--- sreejita/cli.py
+++ sreejita/cli.py
@@ -2,6 +2,8 @@ import argparse
 from sreejita.config.loader import load_config
 from sreejita.reports.hybrid import run
 
+parser.add_argument("--batch", help="Run batch mode on folder")
+
 def main():
     parser = argparse.ArgumentParser("Sreejita Framework v1.2")
     parser.add_argument("input")
@@ -10,3 +12,8 @@ def main():
     args = parser.parse_args()
     config = load_config(args.config)
     run(args.input, config)
+    
+    if args.batch:
+    from sreejita.automation.batch_runner import run_batch
+    run_batch(args.batch, args.config)
+    return
