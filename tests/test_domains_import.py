3e88c672f6e4bf6cf7089992a1973dd000333451
Arasavalli Yeswanth Nagasai <132380899+YeswanthArasavalli@users.noreply.github.com>
2025-12-14 09:34:26 +0530

    Create test_domains_import.py

---
+++ tests/test_domains_import.py
@@ -0,0 +1,11 @@
+def test_retail_domain_import():
+    import sreejita.domains.retail
+
+def test_ecommerce_domain_import():
+    import sreejita.domains.ecommerce
+
+def test_customer_domain_import():
+    import sreejita.domains.customer
+
+def test_text_domain_import():
+    import sreejita.domains.text
