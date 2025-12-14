46baf537a6f5b435644356c09ee2889ddeff57ae
Arasavalli Yeswanth Nagasai <132380899+YeswanthArasavalli@users.noreply.github.com>
2025-12-14 14:31:26 +0530

    Update test_automation_import.py

--- tests/test_automation_import.py
+++ tests/test_automation_import.py
@@ -1,8 +1,10 @@
 def test_batch_runner_import():
     import sreejita.automation.batch_runner
 
+
 def test_file_watcher_import():
     import sreejita.automation.file_watcher
+
 
 def test_scheduler_import():
     import sreejita.automation.scheduler
