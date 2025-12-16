8d41f0df7b702e1410251189ac4809f5906275cf
sreejitadatalabs <sreejitadatalabs@gmail.com>
2025-12-16 15:15:01 +0530

    Update registry.py

--- sreejita/reporting/registry.py
+++ sreejita/reporting/registry.py
@@ -2,6 +2,19 @@ from sreejita.reporting.retail.kpis import compute_retail_kpis
 from sreejita.reporting.retail.insights import generate_retail_insights
 from sreejita.reporting.retail.recommendations import generate_retail_recommendations
 
+from sreejita.reporting.retail.visuals import (
+    shipping_cost_vs_sales,
+    discount_distribution,
+)
+
+DOMAIN_VISUALS = {
+    "retail": {
+        "shipping_cost_ratio": shipping_cost_vs_sales,
+        "average_discount": discount_distribution,
+    }
+}
+
+
 DOMAIN_REPORT_ENGINES = {
     "retail": {
         "kpis": compute_retail_kpis,
