c21e9ebeabcae2bc3c550fa44df33c007059ff55
sreejitadatalabs <sreejitadatalabs@gmail.com>
2025-12-16 12:04:53 +0530

    Create test_decision_replay.py

---
+++ tests/test_decision_replay.py
@@ -0,0 +1,7 @@
+def test_domain_decision_is_deterministic(sample_df):
+    d1 = decide_domain(sample_df)
+    d2 = decide_domain(sample_df)
+
+    assert d1.selected_domain == d2.selected_domain
+    assert d1.confidence == d2.confidence
+    assert d1.fingerprint == d2.fingerprint
