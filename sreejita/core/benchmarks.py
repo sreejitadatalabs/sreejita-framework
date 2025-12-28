baec1b142ee3e93671d463493ddaf302a26cbd3e
sreejitadatalabs <sreejitadatalabs@gmail.com>
2025-12-28 21:19:50 +0530

    Create benchmarks.py

---
+++ sreejita/core/benchmarks.py
@@ -0,0 +1,15 @@
+BENCHMARKS = {
+    "healthcare": {
+        "avg_los": 4.5,
+        "readmission_rate": 0.10,
+        "long_stay_rate": 0.15
+    },
+    "retail": {
+        "stockout_rate": 0.05,
+        "gmroi": 1.2
+    },
+    "marketing": {
+        "roas": 3.0,
+        "ctr": 0.01
+    }
+}
